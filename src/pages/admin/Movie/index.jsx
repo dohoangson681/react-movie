f6eeb88391c53017743052ea4d06f1f73d2b4614
Son <dohoangson060801@gmail.com>
2022-11-18 14:16:41 +0700

    m

---
+++ src/pages/admin/Movie/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './index.css' ; 
+export default function AdminMovie() {
+  return (
+    <div>AdminMovie</div>
+  )
+}
--- src/service/QLNDService/QLNDService.js
+++ src/service/QLNDService/QLNDService.js
@@ -2,6 +2,18 @@ import {  GP_ID } from "../../util/setting";
 import { http } from '../../util/config';
 
 export default class QLNDService {
+    layDSLoaiND = () => {
+        return http.get('/api/QuanLyNguoiDung/LayDanhSachLoaiNguoiDung') ;
+    }
+    layDSND = (groupID) => {
+        return http.get(`/api/QuanLyNguoiDung/LayDanhSachNguoiDung?MaNhom=${groupID}`) ;
+    }
+    timKiemND = (taiKhoan) => {
+        return http.get(`/api/QuanLyNguoiDung/TimKiemNguoiDung?MaNhom=GP01&tuKhoa=${taiKhoan}`) ; 
+    }
+    layInfoND = (taiKhoan) => {
+        return http.post (`/api/QuanLyNguoiDung/LayThongTinNguoiDung?taiKhoan=${taiKhoan}`)
+    }
     dangNhap = (thongTinDangNhap) => {
         return http.post(`/api/QuanLyNguoiDung/DangNhap`,thongTinDangNhap)
     };
